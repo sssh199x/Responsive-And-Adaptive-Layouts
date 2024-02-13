3d8cc33fdde8bfe7ebbc5ce73de98e5367ab148a
sssh199x <sandeshhamal5890@gmail.com>
2024-02-13 22:36:28 +0545

    Refactor

--- lib/screens/dashboard/components/my_files.dart
+++ lib/screens/dashboard/components/my_files.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsiveness/constants.dart';
+import 'package:responsiveness/models/My_files.dart';
+import 'package:responsiveness/screens/dashboard/components/file_info_card.dart';
+import 'package:responsiveness/screens/dashboard/components/recent_files_view.dart';
 
 class MyFiles extends StatelessWidget {
   const MyFiles({
@@ -8,28 +11,54 @@ class MyFiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Text(
-          'My Files',
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        ElevatedButton.icon(
-            style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: defaultPadding * 1.5,
-                    vertical: defaultPadding)),
-            onPressed: () {},
-            icon: const Icon(
-              Icons.add,
-              color: bgColorLight,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'My Files',
+              style: Theme.of(context).textTheme.titleMedium,
             ),
-            label: const Text(
-              'Add New',
-              style: TextStyle(color: bgColorLight),
-            )),
+            ElevatedButton.icon(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: defaultPadding * 1.5,
+                        vertical: defaultPadding)),
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  color: bgColorLight,
+                ),
+                label: const Text(
+                  'Add New',
+                  style: TextStyle(color: bgColorLight),
+                )),
+          ],
+        ),
+        const SizedBox(
+          height: defaultPadding,
+        ),
+        GridView.builder(
+          shrinkWrap: true,
+          itemCount: demoMyFiles.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              mainAxisSpacing: defaultPadding,
+              crossAxisSpacing: defaultPadding,
+              childAspectRatio: 1.3),
+          itemBuilder: (context, index) {
+            return FileInfoCard(
+              info: demoMyFiles[index],
+            );
+          },
+        ),
+        const SizedBox(
+          height: defaultPadding,
+        ),
+        const RecentFilesView()
       ],
     );
   }
