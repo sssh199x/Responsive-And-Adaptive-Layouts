c1ee06e5bad3387f10d9f68a2b521c630a14362e
sssh199x <sandeshhamal5890@gmail.com>
2024-02-15 16:40:13 +0545

    fix:Responsive header_section

--- lib/screens/dashboard/components/header_section.dart
+++ lib/screens/dashboard/components/header_section.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 import 'package:responsiveness/constants.dart';
+import 'package:responsiveness/controllers/menu_app_controller.dart';
+import 'package:responsiveness/responsive.dart';
 
 class HeaderSection extends StatelessWidget {
   const HeaderSection({super.key});
@@ -9,10 +12,17 @@ class HeaderSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text('DashBoard', style: Theme.of(context).textTheme.headlineMedium),
-        const Spacer(
-          flex: 2,
-        ),
+        if (!Responsive.isDesktop(context))
+          IconButton(
+              onPressed: context.read<MenuAppController>().controlMenu,
+              icon: const Icon(
+                Icons.menu,
+                color: bgColorLight,
+              )),
+        if (!Responsive.isMobile(context))
+          Text('DashBoard', style: Theme.of(context).textTheme.headlineMedium),
+        if (!Responsive.isMobile(context))
+          Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
         const Expanded(
           child: SearchField(),
         ),
@@ -31,7 +41,7 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(
-          horizontal: defaultPadding, vertical: defaultPadding / 2.5),
+          horizontal: defaultPadding, vertical: defaultPadding / 2),
       margin: const EdgeInsets.only(left: defaultPadding),
       decoration: BoxDecoration(
           color: secondaryColor,
@@ -45,10 +55,11 @@ class ProfileView extends StatelessWidget {
               'assets/images/profile_pic.png',
               height: 38,
             ),
-            const Padding(
-              padding: EdgeInsets.all(defaultPadding / 2),
-              child: Text('Scarlet Johnasson'),
-            ),
+            if (!Responsive.isMobile(context))
+              const Padding(
+                padding: EdgeInsets.all(defaultPadding / 2),
+                child: Text('Scarlet Johnasson'),
+              ),
             const Icon(
               Icons.keyboard_arrow_down,
               color: bgColorLight,
@@ -67,23 +78,50 @@ class SearchField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SearchBar(
-      textStyle: const MaterialStatePropertyAll(TextStyle(color: bgColorLight)),
-      trailing: [
-        InkWell(
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search",
+        hintStyle: const TextStyle(color: bgColorLight),
+        fillColor: secondaryColor,
+        filled: true,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        suffixIcon: InkWell(
           onTap: () {},
           child: Container(
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(10)),
             padding: const EdgeInsets.all(defaultPadding * 0.75),
             margin: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-            child: SvgPicture.asset('assets/icons/Search.svg'),
+            decoration: const BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: SvgPicture.asset("assets/icons/Search.svg"),
           ),
-        )
-      ],
-      backgroundColor: const MaterialStatePropertyAll(secondaryColor),
-      hintText: 'Search',
-      hintStyle: const MaterialStatePropertyAll(TextStyle(color: bgColorLight)),
+        ),
+      ),
     );
+    // SearchBar(
+    //   textStyle: const MaterialStatePropertyAll(TextStyle(color: bgColorLight)),
+    //   trailing: [
+    //     InkWell(
+    //       onTap: () {},
+    //       child: Container(
+    //         decoration: BoxDecoration(
+    //             color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+    //         padding: const EdgeInsets.all(defaultPadding * 0.5),
+    //         margin: const EdgeInsets.symmetric(horizontal: defaultPadding / 4),
+    //         child: SvgPicture.asset(
+    //           'assets/icons/Search.svg',
+    //           fit: BoxFit.contain,
+    //         ),
+    //       ),
+    //     )
+    //   ],
+    //   backgroundColor: const MaterialStatePropertyAll(secondaryColor),
+    //   hintText: 'Search',
+    //   hintStyle: const MaterialStatePropertyAll(TextStyle(color: bgColorLight)),
+    // );
   }
 }
