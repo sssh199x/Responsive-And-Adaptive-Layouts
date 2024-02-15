ebdf4db9711c186088068890849311dcfe7ce67b
sssh199x <sandeshhamal5890@gmail.com>
2024-02-15 16:33:05 +0545

    Add: Never Scrollable physics

--- lib/screens/dashboard/components/file_info_card_grid_view.dart
+++ lib/screens/dashboard/components/file_info_card_grid_view.dart
@@ -15,6 +15,7 @@ class FileInfoCardGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
       shrinkWrap: true,
       itemCount: demoMyFiles.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
