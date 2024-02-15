eecd2099bc57889c6e26d04e4e785ad631501e18
sssh199x <sandeshhamal5890@gmail.com>
2024-02-15 16:32:14 +0545

    Refactor

--- lib/models/my_files.dart
+++ lib/models/my_files.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:responsiveness/constants.dart';
 
@@ -50,4 +51,19 @@ List demoMyFiles = [
     color: const Color(0xFF007EE5),
     percentage: 0.78,
   ),
+];
+List<PieChartSectionData> pieChartSectionData = [
+  PieChartSectionData(
+      radius: 25, value: 25, showTitle: false, color: primaryColor),
+  PieChartSectionData(
+      radius: 22, value: 20, color: const Color(0xFF26E5FF), showTitle: false),
+  PieChartSectionData(
+      radius: 19, value: 10, color: const Color(0xFFFFCF26), showTitle: false),
+  PieChartSectionData(
+      radius: 16, value: 15, color: const Color(0xFFEE2727), showTitle: false),
+  PieChartSectionData(
+      showTitle: false,
+      radius: 13,
+      value: 25,
+      color: primaryColor.withOpacity(0.1))
 ];
