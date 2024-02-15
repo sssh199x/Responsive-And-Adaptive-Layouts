3bfb85d0e6a2efb23678ce682dc036e4b6b7d1a6
sssh199x <sandeshhamal5890@gmail.com>
2024-02-15 16:42:14 +0545

    Fix : Made Data table scrollable

--- lib/screens/dashboard/components/recent_files_view.dart
+++ lib/screens/dashboard/components/recent_files_view.dart
@@ -10,6 +10,7 @@ class RecentFilesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(defaultPadding),
@@ -23,25 +24,51 @@ class RecentFilesView extends StatelessWidget {
             style: Theme.of(context).textTheme.bodyMedium,
           ),
           SizedBox(
-            width: double.infinity,
-            child: DataTable(
-              columns: const [
-                DataColumn(
-                  label: Text('File Name',
-                      maxLines: 1, overflow: TextOverflow.ellipsis),
-                ),
-                DataColumn(
-                  label: Text('Date',
-                      maxLines: 1, overflow: TextOverflow.ellipsis),
-                ),
-                DataColumn(
-                  label: Text('Size',
-                      maxLines: 1, overflow: TextOverflow.ellipsis),
-                ),
-              ],
-              rows: List.generate(demoRecentFiles.length,
-                  (index) => recentFileDataRow(demoRecentFiles[index])),
-            ),
+            width: double.maxFinite,
+            child: size.width <= 375
+                ? SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      border: TableBorder.all(color: bgColorLight),
+                      columnSpacing: 15,
+                      columns: const [
+                        DataColumn(
+                          label: Text('File Name',
+                              maxLines: 1, overflow: TextOverflow.ellipsis),
+                        ),
+                        DataColumn(
+                          label: Text('Date',
+                              maxLines: 1, overflow: TextOverflow.ellipsis),
+                        ),
+                        DataColumn(
+                          label: Text('Size',
+                              maxLines: 1, overflow: TextOverflow.ellipsis),
+                        ),
+                      ],
+                      rows: List.generate(demoRecentFiles.length,
+                          (index) => recentFileDataRow(demoRecentFiles[index])),
+                    ),
+                  )
+                : DataTable(
+                    border: TableBorder.all(color: bgColorLight),
+                    columnSpacing: 15,
+                    columns: const [
+                      DataColumn(
+                        label: Text('File Name',
+                            maxLines: 1, overflow: TextOverflow.ellipsis),
+                      ),
+                      DataColumn(
+                        label: Text('Date',
+                            maxLines: 1, overflow: TextOverflow.ellipsis),
+                      ),
+                      DataColumn(
+                        label: Text('Size',
+                            maxLines: 1, overflow: TextOverflow.ellipsis),
+                      ),
+                    ],
+                    rows: List.generate(demoRecentFiles.length,
+                        (index) => recentFileDataRow(demoRecentFiles[index])),
+                  ),
           )
         ],
       ),
@@ -52,23 +79,32 @@ class RecentFilesView extends StatelessWidget {
 DataRow recentFileDataRow(RecentFile infoFile) {
   return DataRow(cells: [
     DataCell(Row(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         SvgPicture.asset(
           infoFile.icon!,
           height: 30,
-          width: 30,
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-          child: Text(infoFile.title!),
+          padding: const EdgeInsets.symmetric(horizontal: 0),
+          child: Text(
+            infoFile.title!,
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       ],
     )),
     DataCell(
-      Text(infoFile.date!),
+      Text(
+        infoFile.date!,
+        overflow: TextOverflow.ellipsis,
+      ),
     ),
     DataCell(
-      Text(infoFile.size!),
+      Text(
+        infoFile.size!,
+        overflow: TextOverflow.ellipsis,
+      ),
     ),
   ]);
 }
